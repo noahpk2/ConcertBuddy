81e09baea087926e4eeaef38ad3b87bdc3d4c2d6
noahpk2 <noah@pearsonkramer.com>
2023-07-06 12:20:09 -0600

    Toolbar and nav drawer added- but the nav toggle only works sometimes?

--- app/src/main/java/com/example/concertbuddy/MainActivity.kt
+++ app/src/main/java/com/example/concertbuddy/MainActivity.kt
@@ -3,21 +3,49 @@ package com.example.concertbuddy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.example.concertbuddy.CalendarFragment
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity"
     }
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        drawerLayout = findViewById(R.id.my_drawer_layout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close)
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        //setSupportActionBar(toolbar)
+        // to make the Navigation drawer icon always appear on the action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val fragment = CalendarFragment()
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, fragment)
             .commit()
         Log.d(TAG, "onCreate: ")
+
+        //
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            Log.d(TAG, "onOptionsItemSelected: Toggle Selected")
+            true
+        } else super.onOptionsItemSelected(item)
     }
 }
