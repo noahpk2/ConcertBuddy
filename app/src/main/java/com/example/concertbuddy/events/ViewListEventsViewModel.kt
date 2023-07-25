7a8f05f9a9e6a648da4721337187dc65b8ccc2b1
sam Towne <stowne#@msudenver.edu>
2023-07-24 20:50:03 -0600

    work in progress

--- app/src/main/java/com/example/concertbuddy/events/ViewListEventsViewModel.kt
+++ app/src/main/java/com/example/concertbuddy/events/ViewListEventsViewModel.kt
@@ -9,3 +9,17 @@ import com.example.concertbuddy.events.calendarData
 class ViewListEventsViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
+
+class ViewListEventsViewModelFactory(private val repository: EventRepository) : ViewModelProvider.Factory {
+    /**
+     * This class is responsible for creating the ViewListEventsViewModel, because the default ViewModel constructor
+     * does not allow us to pass in any parameters.
+     */
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ViewListEventsViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ViewListEventsViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
